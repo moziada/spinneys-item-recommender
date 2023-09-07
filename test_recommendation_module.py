f7f703f769e1fc1d4f84b8b2d77519195e92808a
Mohamed Ziada <mohamed.ziada74@gmail.com>
2023-09-07 16:09:16 +0300

    editing tests according to new metric

--- test_recommendation_module.py
+++ test_recommendation_module.py
@@ -1,7 +1,6 @@
 import pytest
 import numpy as np
 import pandas as pd
-from operator import itemgetter
 
 from recommendation_module import Item2Item
 
@@ -12,19 +11,18 @@ def test_calc_item2item_matrix():
         "Item No_": ["10", "11", "12", "13", "10", "11", "10"]})
     model.calc_item2item_matrix(df, user_col="Receipt No_", item_col="Item No_")
 
-    result = np.array([[0, 0.4, 0.25, 0.25], [0.4, 0, 1/3, 1/3], [0.25, 1/3, 0, 0.5], [0.25, 1/3, 0.5, 0]])
+    result = np.array([[0, 2/3, 1/3, 1/3], [2/3, 0, 0.5, 0.5], [1/3, 0.5, 0, 1], [1/3, 0.5, 1, 0]])
     assert np.allclose(result, model.item2item_matrix.toarray())
 
 
 def test_get_top_n_frequent_items():
-    model = Item2Item(load_dir="MOA-Jul-optimized-V01")  # Create an instance of your class
+    model = Item2Item(load_dir="MOA-Jul-optimized-freq_adjusted-V01")
     expected_result = {
-        'items': ['111195', '388101', '287238', '102248', '112212'],
-        'scores': np.array([70, 71, 82, 85, 86])
+        'items': ['102248', '101695', '137346', '137220', '102251'],
+        'scores': np.array([0.05806011, 0.04654545, 0.04359926, 0.04179663, 0.0415601])
         }
     
     result = model.get_top_n_frequent_items("100757", n=5)
-    order = np.argsort(result['scores'])
 
-    assert result['items'] == list(itemgetter(*order)(expected_result['items']))
-    assert np.array_equal(result['scores'], expected_result['scores'][order])
+    assert result['items'] == expected_result['items']
+    assert np.allclose(result['scores'], expected_result['scores'])
