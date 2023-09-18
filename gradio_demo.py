351adf41723094d00f037510f7a4e0742cdbaf77
Mohamed Ziada <mohamed.ziada74@gmail.com>
2023-09-18 12:48:56 +0300

    filtering results WIP

--- gradio_demo.py
+++ gradio_demo.py
@@ -3,6 +3,7 @@ import plotly.express as px
 import os
 import pandas as pd
 from recommendation_module import *
+import utils
 
 def load_items():
     df = pd.read_excel("data/products info (shortlist).xlsx")
@@ -11,8 +12,9 @@ def load_items():
     df.set_index("Item Code", inplace=True)
     return df.to_dict(orient="index")
 
-def bar_plot_fn(exclude_subgroup, item_code, n):
-    output = model.get_top_n_frequent_items(item_code, n=int(n), exclude_subgroup=exclude_subgroup)
+def bar_plot_fn(item_code, exclude_subgroup, exclude_product_group, item_categories, n):
+    output = model.get_top_n_frequent_items(item_code, n=int(n), exclude_subgroup=False)
+    output = utils.post_ranking(item_code, output, exclude_subgroup, exclude_product_group, item_categories)
     print(output)
     df = pd.DataFrame(
             {
@@ -25,8 +27,8 @@ def bar_plot_fn(exclude_subgroup, item_code, n):
 
 
 #available_models = os.listdir("models/item2item")
-#model = Item2Item(load_dir="1-year")
-#items_mapper = load_items()
+model = Item2Item(load_dir="1-year")
+items_mapper = load_items()
 
 with gr.Blocks(theme=gr.themes.Default(text_size=gr.themes.sizes.text_lg)) as demo:
     with gr.Column():
@@ -35,16 +37,13 @@ with gr.Blocks(theme=gr.themes.Default(text_size=gr.themes.sizes.text_lg)) as de
             with gr.Column():
                 exclude_subgroup = gr.Checkbox(label="Exclude Subgroup")
                 exclude_product_group = gr.Checkbox(label="Exclude Product Group")
-            item_categories = gr.Dropdown([1, 2, 3], label="Item Category", multiselect=True)
+            item_categories = gr.Dropdown(utils.items_info_df["Category Code"].unique(), label="Item Category", multiselect=True)
             num_of_recommendations = gr.Number(5, label="Number of Recommendations")
         with gr.Row():
             submit = gr.Button()
         prod_name = gr.Textbox(label="Item Name")
         plot = gr.Plot()
 
-    submit.click(bar_plot_fn, inputs=[exclude_subgroup, item_code, num_of_recommendations], outputs=[plot, prod_name])
-    #exclude_subgroup.change(bar_plot_fn, inputs=[exclude_subgroup, item_code, num_of_recommendations], outputs=[plot, prod_name])
-    #num_of_recommendations.change(bar_plot_fn, inputs=[exclude_subgroup, item_code, num_of_recommendations], outputs=[plot, prod_name])
-    #item_code.change(bar_plot_fn, inputs=[exclude_subgroup, item_code], outputs=plot)
+    submit.click(bar_plot_fn, inputs=[item_code, exclude_subgroup, exclude_product_group, item_categories, num_of_recommendations], outputs=[plot, prod_name])
 
 demo.launch()
