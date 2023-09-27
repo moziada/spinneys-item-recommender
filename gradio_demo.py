21477785030aedeb29c9b0fb99d9bde7f99789c4
Mohamed Ziada <mohamed.ziada74@gmail.com>
2023-09-27 11:15:18 +0300

    add filters to the demo

--- gradio_demo.py
+++ gradio_demo.py
@@ -3,6 +3,7 @@ import plotly.express as px
 import os
 import pandas as pd
 from recommendation_module import *
+import utils
 
 def load_items():
     df = pd.read_excel("data/products info (shortlist).xlsx")
@@ -11,9 +12,11 @@ def load_items():
     df.set_index("Item Code", inplace=True)
     return df.to_dict(orient="index")
 
-def bar_plot_fn(exclude_subgroup, item_code, n, min_support, min_confidence, min_lift):
-    output = model.get_top_n_frequent_items(item_code, n=int(n), exclude_subgroup=exclude_subgroup, min_support=min_support, min_confidence=min_confidence, min_lift=min_lift)
-    print(output)
+def bar_plot_fn(item_code, exclude_subgroup, exclude_product_group, items_per_subgroup_limit, item_categories, n, min_support, min_confidence, min_lift):
+    output = model.get_top_n_frequent_items(item_code, n=int(n), exclude_subgroup=False, min_support=min_support, min_confidence=min_confidence, min_lift=min_lift)
+    output = utils.post_ranking(item_code, output, exclude_subgroup, exclude_product_group, item_categories)
+    if items_per_subgroup_limit:
+        output = utils.max_items_per_subgroup(model_out=output, n=1)
     df = pd.DataFrame(
             {
             "items": [items_mapper[i_code]["Item"] + " (" + str(items_mapper[i_code]["Product Size"]) + ")" for i_code in output["items"]],
@@ -28,23 +31,26 @@ def bar_plot_fn(exclude_subgroup, item_code, n, min_support, min_confidence, min
 model = Item2Item(load_dir="1-year-V02")
 items_mapper = load_items()
 
-with gr.Blocks(theme=gr.themes.Default(text_size=gr.themes.sizes.text_lg)) as demo:
+with gr.Blocks() as demo:
     with gr.Column():
         with gr.Row():
-            item_code = gr.Textbox(label="Item Code")
-            exclude_subgroup = gr.Checkbox(label="Exclude Subgroup")
             num_of_recommendations = gr.Number(5, label="Number of Recommendations")
-            min_support = gr.Number(0.01, label="min_support", step=0.05)
-            min_cofidence = gr.Number(0.1, label="min_confidence", step=0.05)
+            min_support = gr.Number(0.0001, label="min_support", step=0.05)
+            min_confidence = gr.Number(0.01, label="min_confidence", step=0.05)
             min_lift = gr.Number(1, label="min_lift", step=0.05)
+        with gr.Row():
+            item_code = gr.Textbox(label="Item Code")
+            with gr.Column():
+                exclude_subgroup = gr.Checkbox(label="Exclude Subgroup")
+                exclude_product_group = gr.Checkbox(label="Exclude Product Group")
+                items_per_subgroup_limit = gr.Checkbox(label="Items Per Subgroup Limit")
+        unique_categories = utils.items_info_df["Category Code"].unique().tolist()
+        item_categories = gr.Dropdown(choices=unique_categories, value=unique_categories, label="Item Category", multiselect=True)
         with gr.Row():
             submit = gr.Button()
         prod_name = gr.Textbox(label="Item Name")
         plot = gr.Plot()
 
-    submit.click(bar_plot_fn, inputs=[exclude_subgroup, item_code, num_of_recommendations, min_support, min_cofidence, min_lift], outputs=[plot, prod_name])
-    #exclude_subgroup.change(bar_plot_fn, inputs=[exclude_subgroup, item_code, num_of_recommendations], outputs=[plot, prod_name])
-    #num_of_recommendations.change(bar_plot_fn, inputs=[exclude_subgroup, item_code, num_of_recommendations], outputs=[plot, prod_name])
-    #item_code.change(bar_plot_fn, inputs=[exclude_subgroup, item_code], outputs=plot)
+    submit.click(bar_plot_fn, inputs=[item_code, exclude_subgroup, exclude_product_group, items_per_subgroup_limit, item_categories, num_of_recommendations, min_support, min_confidence, min_lift], outputs=[plot, prod_name])
 
 demo.launch()
