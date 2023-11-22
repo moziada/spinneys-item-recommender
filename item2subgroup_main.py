7f3440678a381ac5cf2f6ea45610059a06dde9d0
Mohamed Ziada <mohamed.ziada74@gmail.com>
2023-11-22 15:08:38 +0200

    removing old code

--- item2subgroup_main.py
+++
@@ -1,6 +0,0 @@
-from recommendation_module import Item2Subgroup
-import pandas as pd
-
-df = pd.read_csv("data/MOA-JUL V02.csv")
-model = Item2Subgroup()
-model.calc_item2subgroup_matrix(df, save_dir="MOA - Jul", item_col="Item No_", receipt_col="Receipt No_", subgroup_col="Item Sub Group Code")
--- test.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-import os
-
-#df = pd.read_csv("data/MOA-JUL V02.csv")
-# print(df.drop_duplicates(subset=["Item No_", "Item Sub Group Code"]).head().to_dict(orient="list"))
-#print(df[["Item No_", "Item Sub Group Code"]].drop_duplicates(subset=["Item No_"]).set_index("Item No_").head().to_dict())
-os.makedirs("models\\item2subgroup\\MOA - Jul")
